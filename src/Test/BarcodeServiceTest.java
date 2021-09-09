4d123e00dd18da7f039a7decce709b53e4835df7
MarMaksk <87898650+MarMaksk@users.noreply.github.com>
2021-09-09 21:31:04 +0300

    Create test for BarcodeService

---
+++ src/Test/BarcodeServiceTest.java
@@ -0,0 +1,22 @@
+package Test;
+
+import Task.Product;
+import Task.Service.BarcodeService;
+import Task.enums.ActionType;
+import Task.enums.UnitType;
+import Task.exception.BarcodeException;
+import Task.exception.NameProductException;
+import Task.exception.PriceProductException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BarcodeServiceTest {
+
+    @Test
+    void searchBarcode() throws BarcodeException, PriceProductException, NameProductException {
+        Product product = new Product("Dirol", 1, 1, UnitType.gram, "1000000000014",
+                ActionType.SEASON_SALE);
+        assertEquals(product.getBarcode(), new BarcodeService().searchBarcode("1000000000014").getBarcode());
+    }
+}
